036073b879e3c93f3aca19388ffa05200e407d22
Gowtham Potnuru <gouthamcall@gmail.com>
2022-11-28 12:34:42 -0500

    udpated

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+ || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
 HelloController(app)
 UserController(app)
-TuitsController(app);
+// TuitsController(app);
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log('Express server listening on port', port)
